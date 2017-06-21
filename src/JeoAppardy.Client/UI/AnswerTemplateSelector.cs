9a42378ce1be725970f6ff0a676b274e506d13bf
Jan Karger <punkerat76@gmail.com>
2017-06-21 16:04:17 +0200

    show asset images

---
+++ src/JeoAppardy.Client/UI/AnswerTemplateSelector.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using JeoAppardy.Client.Api;
+
+namespace JeoAppardy.Client.UI
+{
+  public class AnswerTemplateSelector : DataTemplateSelector
+  {
+    public DataTemplate TextTemplate { get; set; }
+    public DataTemplate FileTemplate { get; set; }
+    public DataTemplate ImageTemplate { get; set; }
+
+    protected override DataTemplate SelectTemplateCore(object item, DependencyObject container)
+    {
+      var answer = item as Api.Answer;
+      if (answer != null)
+      {
+        switch (answer.Type)
+        {
+          case AnswerType.File:
+            return FileTemplate;
+          case AnswerType.Image:
+            return ImageTemplate;
+          case AnswerType.Text:
+          default:
+            return TextTemplate;
+        }
+      }
+      return base.SelectTemplateCore(item, container);
+    }
+
+    protected override DataTemplate SelectTemplateCore(object item)
+    {
+      return base.SelectTemplateCore(item);
+    }
+  }
+}
---
+++ src/JeoAppardy.Client/UI/AssetValueConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Windows.ApplicationModel;
+using Windows.UI.Xaml.Data;
+using JeoAppardy.Client.Api;
+
+namespace JeoAppardy.Client.UI
+{
+  public class AssetValueConverter : IValueConverter
+  {
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+      var answer = value as Api.Answer;
+      if (answer != null)
+      {
+        var asset = answer.Asset;
+        var installedLocation = Package.Current.InstalledLocation;
+        switch (answer.Type)
+        {
+          case AnswerType.File:
+            return $"{installedLocation.Path}\\Assets\\{asset}";
+          case AnswerType.Image:
+            return $"{installedLocation.Path}\\Assets\\{asset}";
+          case AnswerType.Text:
+          default:
+            return asset;
+        }
+      }
+      return "uuups";
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
